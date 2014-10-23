9cea4494a5c290750135152789dc629e392c8e16
Jake <jake0oo0andminecraft@gmail.com>
2014-10-23 15:43:32 -0500

    Username -> UUID

--- lib/minecraft_auth.rb
+++ lib/minecraft_auth.rb
@@ -10,6 +10,18 @@ module MinecraftAuth
     Account.new(user, pass)
   end
 
+  def self.username_to_uuid(username)
+    response = HTTParty.post("https://api.mojang.com/profiles/minecraft",
+      :body => [username].to_json,
+      :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json'}
+      )
+    if response.length >= 1
+      return response[0]["id"]
+    else
+      return nil
+    end
+  end
+
   class Account
     attr_reader :username
     attr_reader :password
@@ -23,15 +35,15 @@ module MinecraftAuth
 
       response = HTTParty.post("#{PROTOCOL}#{BASE_URL}#{ENDPOINT}",
         :body => {
-            :"agent" => {
+          :"agent" => {
             :"name" => "Minecraft",
             :"version" => 1
-          },
-          :"username" => "#{@username}",
-          :"password" => "#{@password}"
-        }.to_json,
-        :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json'}
-      )
+            },
+            :"username" => "#{@username}",
+            :"password" => "#{@password}"
+            }.to_json,
+            :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json'}
+            )
       # No Error
       if not response['error']
         @access_token = response['accessToken']
@@ -44,27 +56,27 @@ module MinecraftAuth
           @profiles << Profile.new(profile['name'], profile['id'], selected)
         end
       # Error in response  
-      else
-        raise AccountError, response["errorMessage"]
-      end
+    else
+      raise AccountError, response["errorMessage"]
     end
   end
+end
 
-  class Profile
-    attr_reader :name
-    attr_reader :id
-    attr_reader :selected
+class Profile
+  attr_reader :name
+  attr_reader :id
+  attr_reader :selected
 
-    def initialize(name, id, selected)
-      @id = id
-      @name = name
-      @selected = selected
-    end
+  def initialize(name, id, selected)
+    @id = id
+    @name = name
+    @selected = selected
   end
+end
 
-  class AccountError < StandardError
-    def initialize(message)
-      super()
-    end
+class AccountError < StandardError
+  def initialize(message)
+    super()
   end
+end
 end
--- test/test_account.rb
+++ test/test_account.rb
@@ -8,6 +8,14 @@ class AccountTest < Test::Unit::TestCase
     end
   end
 
+  def test_uuid
+    assert_equal "2d5aa9cdaeb049189930461fc9b91cc5", MinecraftAuth.username_to_uuid('Jake0oo0')
+  end
+
+  def test_bad_uuid
+    assert_nil MinecraftAuth.username_to_uuid('DoesNotExistUser')
+  end
+
   # This can be tested if necessary, otherwise credentials should remain private.
   # def test_valid
   #   assert_raise MinecraftAuth::AccountError do
